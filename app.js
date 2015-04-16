9bcb1e64dad171d3052f31170c9b83f089a14e04
ChrissiQ <rediahs@gmail.com>
2015-04-16 10:57:43 -0600

    Slightly more concise

--- app.js
+++ app.js
@@ -3,6 +3,6 @@
 import io from'socket.io';
 
 io(3030).on('connection', socket => {
-  socket.on('message', msg => { console.log(`Message: ${msg}`)});
+  socket.on('message', msg => console.log(`Message: ${msg}`));
   socket.on('disconnect', () => { });
 });
