b4d3b4df24e8f69caacbea2b7a57f637427851ef
ChrissiQ <rediahs@gmail.com>
2015-04-16 11:39:55 -0600

    Add mongoose storage and block retrieval/sending

--- app.js
+++ app.js
@@ -3,12 +3,40 @@
 import io from'socket.io';
 import mongoose from 'mongoose';
 import { development as config } from './config';
- 
-io(3030).on('connection', socket => {
-  socket.on('message', msg => console.log(`Message: ${msg}`));
-  socket.on('disconnect', () => { });
-});
 
-// Connect to mongodb
 mongoose.connect(config.db);
 const db = mongoose.connection;
+
+db.on('error', err => console.error(`Connection error: ${err}`));
+db.on('disconnected', () => mongoose.connect(config.db));
+db.on('open', () => {
+
+  let BlockSchema = new mongoose.Schema({
+    x: {type : Number},
+    y: {type : Number},
+    h: {type : Number},
+    createdAt: {type : Date, default : Date.now}
+  });
+
+  mongoose.model('Block', BlockSchema);
+  
+  let Block = mongoose.model('Block');
+
+  io(3030).on('connection', socket => {
+    socket.on('newBlock', data => {
+
+      Block.findOne( { x:data.x, y:data.y }, (err,block) => {
+
+        let emitNewBlock = (err,block) => io.sockets.emit('newBlock', block);
+
+        if (!block)
+          Block.create(data, emitNewBlock);
+        else {
+          delete data._id;
+          Block.findByIdAndUpdate(block._id, data, emitNewBlock);
+        }
+
+      });
+    });
+  });
+});
